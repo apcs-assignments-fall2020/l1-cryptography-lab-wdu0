63edef15ac388568a78973791a8ec301bd66714a
wdu0 <23wdu@athenian.org>
2020-09-22 07:42:47 +0000

    Added some code to Vigenere methods (incomplete)

--- src/main/java/Vigenere.java
+++ src/main/java/Vigenere.java
@@ -2,13 +2,103 @@ import java.util.Scanner;
 
 public class Vigenere {
     public static String encryptVigenere(String message, String key) {
-        return message;
-        // REPLACE THIS WITH YOUR CODE
+        String newmsg = "";
+        int len = message.length();
+        int p = 0;
+
+        while (p < len) {
+            for (int k = 0; k < key.length();) {
+                char key_ch = key.charAt(k);
+                int keynum = key_ch;
+
+                for (int i = 0; i < len; i++) {
+                    char ch = message.charAt(i);
+
+                    if (i < 65 || (i > 90 && i < 97) || i > 122) {
+                        newmsg += ch;
+                        p++;
+                    } else {
+                        while (keynum >= 26) {
+                            keynum -= 26;
+                        }
+                        if (ch >= 65 && ch <= (90-keynum)) {
+                            ch += keynum;
+                            newmsg += ch;
+                            k++;
+                            p++;
+                        } else if (ch >= (90-keynum+1) && ch <= 90) {
+                            ch -= (26-keynum);
+                            newmsg += ch;
+                            k++;
+                            p++;
+                        } else if (ch >= 97 && ch <= (122-keynum)) {
+                            ch += keynum;
+                            newmsg += ch;
+                            k++;
+                            p++;
+                        } else if (ch >= (122-keynum+1) && ch <= 122) {
+                            ch -= (26-keynum);
+                            newmsg += ch;
+                            k++;
+                            p++;
+                        }
+                    }
+                }
+            }
+        }
+
+        
+        return newmsg;
     }
 
     public static String decryptVigenere(String message, String key) {
-        return message;
-        // REPLACE THIS WITH YOUR CODE
+        String newmsg = "";
+        int len = message.length();
+        int p = 0;
+
+        while (p < len) {
+            for (int k = 0; k < key.length();) {
+                char key_ch = key.charAt(k);
+                int keynum = key_ch;
+
+                for (int i = 0; i < len; i++) {
+                    char ch = message.charAt(i);
+
+                    if (i < 65 || (i > 90 && i < 97) || i > 122) {
+                        newmsg += ch;
+                        p++;
+                    } else {
+                        while (keynum >= 26) {
+                            keynum -= 26;
+                        }
+                        if (ch >= (65+keynum) && ch <= 90) {
+                            ch -= keynum;
+                            newmsg += ch;
+                            k++;
+                            p++;
+                        } else if (ch >= 65 && ch <= (65+keynum-1)) {
+                            ch += (26-keynum);
+                            newmsg += ch;
+                            k++;
+                            p++;
+                        } else if (ch >= (97+keynum) && ch <= 122) {
+                            ch -= keynum;
+                            newmsg += ch;
+                            k++;
+                            p++;
+                        } else if (ch >= 97 && ch <= (97+keynum-1)) {
+                            ch += (26-keynum);
+                            newmsg += ch;
+                            k++;
+                            p++;
+                        }
+                    }
+                }
+            }
+        }
+
+        
+        return newmsg;
     }
 
 
